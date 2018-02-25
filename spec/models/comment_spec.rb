4657a6bcd0b06f32848cad857289cca5246a716c
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-25 17:34:04 +0000

    Scaffold comment + add associations and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+RSpec.describe Comment, type: :model do
   describe "associations" do
     it { should belong_to(:user) }
-    it { should have_many(:comments) }
+    it { should belong_to(:post) }
   end
 
   describe "validation" do
-    it { should validate_presence_of(:caption) }
+    it { should validate_presence_of(:content) }
   end
 end
